96ec78424ae5559070a9f7e191e8bfb3bf7de3ac
Maxvyr <maxvid30@gmail.com>
2021-11-12 16:38:15 +0100

    :tada: web version ok

---
+++ index.js
@@ -0,0 +1,25 @@
+//variable
+const _linkAppStore = "https://apps.apple.com/fr/app/tengu-sensei/id1558435052";
+const _linkPlayStore = "https://play.google.com/store/apps/details?id=com.pandamy.tengu_sensei";
+
+const showStore = (store) => {
+    console.log(store);
+
+    if(store === true){
+        window.open(_linkAppStore, '_blank');
+    } else {
+        window.open(_linkPlayStore, '_blank');
+    }
+}
+
+const instaLink = () => {
+    window.open("https://www.instagram.com/tengusensei_/", '_blank');
+}
+
+const showYear = () => {
+    let _year = new Date().getFullYear();
+    return _year;
+}
+
+
+document.body.innerHTML = document.body.innerHTML.replace("@date", showYear());
