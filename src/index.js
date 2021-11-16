40f747090a0c9fa358cb1e028f0b2c13f8dcd75c
Maxvyr <maxvid30@gmail.com>
2021-11-16 14:49:32 +0100

    :mute: remove log

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const _linkAppStore = "https://apps.apple.com/fr/app/tengu-sensei/id1558435052";
 const _linkPlayStore = "https://play.google.com/store/apps/details?id=com.pandamy.tengu_sensei";
 
 const showStore = (store) => {
-    console.log(store);
-
     if(store === true){
         window.open(_linkAppStore, '_blank');
     } else {
